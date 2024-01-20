f87ef15ce06a0e4d61e5e1d68b9ccf1af3fc2728
Gabrielstav <gabrielstav@yahoo.no>
2024-01-20 17:18:37 +0100

    almost done

--- src/pipeline/significant_interactions.py
+++ src/pipeline/significant_interactions.py
@@ -6,11 +6,15 @@ from src.setup.config_loader import Config
 from src.setup.data_structures import SplineInput
 import dask.dataframe as dd
 from scipy.stats import binom
-# use binom survival function instead? scsp.bdtrc
 
 
 # TODO:
 #   finish implementing these classes
+#   need to handle no bias, inter and intra, normalization by all possible interactions and not normalizing
+#   Just make classes for binomial, wallenius noncentral hypergeometric and fischer noncentral hypergeometric tests
+#   that accepts the data and the splines (if intra --> nchg or binom) and only data (if inter --> fischer) and returns the p-values as new column in the dd.df
+#   then make new classes for fdr and ci (for all data), these are called sequentially from a controller class with access to config, which is called from main pipeline
+#   then make output format class 
 
 class IntraPValueCalculator:
 
@@ -25,9 +29,9 @@ class IntraPValueCalculator:
         self.data["expected_frequency"] = self.data["genomic_distance"].map_partitions(
             lambda x: self.spline(x), meta=("x", "float64"))
 
-    def _calculate_p_values(self):
+    def _calculate_p_values_binom(self):
         # Calculate p-values using a binomial test
-        self.data["p_value"] = self.data.map_partitions(
+        self.data["p_value_binom"] = self.data.map_partitions(
             lambda df: binom.sf(
                 df["interaction_count"] - 1,
                 self.total_interactions,
@@ -36,13 +40,21 @@ class IntraPValueCalculator:
             meta=("x", "float64")
         )
 
+    def calculate_p_valus_nchg(self):
+        # calculate p-values using the nchg test
+        # use wallenius noncentral hypergeometric distribution:
+        self.data["p_value_nchg"] = self.data.map_partitions(
+            lambda df:
+
+
     def run(self):
         self._calculate_expected_frequency()
-        self._calculate_p_values()
+        self._calculate_p_values_binom()
         return self.data[["genomic_distance", "interaction_count", "expected_frequency", "p_value"]]
 
 
 class FDRCalculator:
+
     def __init__(self, p_values):
         self.p_values = p_values
 
@@ -52,6 +64,7 @@ class FDRCalculator:
 
 
 class SignificanceCalculatorController:
+
     def __init__(self, config, intra_data, inter_data):
         self.config = config
         self.intra_data = intra_data
@@ -62,4 +75,3 @@ class SignificanceCalculatorController:
         # using the config, then calculate the FDR
         # then return data by  instantiating a dataclass with the results
         pass
-    
